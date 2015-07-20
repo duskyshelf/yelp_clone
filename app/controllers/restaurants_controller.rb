e84bf8922e8f1e85d263b558c474cf692feeea3d
Zeina Dajani <dajani.zeina@gmail.com>
2015-07-20 17:49:33 +0100

    add and show reviews on individual pages

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,7 +10,7 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.create(restaurant_params)
-    redirect_to '/restaurants'
+    redirect_to restaurants_path
   end
 
   def restaurant_params
@@ -28,13 +28,13 @@ class RestaurantsController < ApplicationController
   def update
     @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
-    redirect_to '/restaurants'
+    redirect_to restaurants_path
   end
 
   def destroy
     @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
     flash[:notice] = 'Restaurant deleted successfully'
-    redirect_to '/restaurants'
+    redirect_to restaurants_path
   end
 end
