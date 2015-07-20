d1ef4f210154bb172e93e5c62d968e637d3196c9
Faisal Choura <faisal.choura@gmail.com>
2015-07-20 12:16:07 +0100

    fixed routing error in test

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'restaurants' do
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
-      visit '/restaurant'
+      visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_content 'Add a restaurant'
     end
