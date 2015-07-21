bfe7cfdd0b481c5985e94b11efacd12b41f54440
Zeina Dajani <dajani.zeina@gmail.com>
2015-07-21 11:11:03 +0100

    started a new branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get 'restaurants' => 'restaurants#index'
+  root to: "restaurants#index"
 
   resources :restaurants do
-    resources :reviews
+    resources :reviews #only: [:show] #except: [:something you don't want aka destory etc.]
   end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
