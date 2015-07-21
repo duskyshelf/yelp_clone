64f40f4345031f6fe33beedb041b3c53d3da94c7
Zeina Dajani <dajani.zeina@gmail.com>
2015-07-21 16:10:56 +0100

    added devise and user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'restaurants' => 'restaurants#index'
-  root to: "restaurants#index"
+  root to: 'restaurants#index'
 
   resources :restaurants do
     resources :reviews #only: [:show] #except: [:something you don't want aka destory etc.]
